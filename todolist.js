960254a4b25b7c1a91d08da899bf66c69f291629
Taron Avagyan <contact@taronavagyan.com>
2022-08-31 11:27:34 -0700

    todolist object

--- todolist.js
+++ todolist.js
@@ -32,3 +32,37 @@ class Todo {
     return this.title;
   }
 }
+
+// This class represents a collection of Todo objects.
+// You can perform typical collection-oriented actions
+// on a TodoList object, including iteration and selection.
+
+class TodoList {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  add(todo) {
+    if (!(todo instanceof Todo))
+      throw new TypeError("can only add Todo objects");
+    this.todos.push(todo);
+  }
+
+  size() {
+    return this.todos.length;
+  }
+}
+
+let list = new TodoList("Today's Todos");
+
+let todo1 = new Todo("Buy milk");
+let todo2 = new Todo("Clean room");
+let todo3 = new Todo("Go to the gym");
+let todo4 = new Todo("Go shopping");
+
+list.add(todo1);
+list.add(todo2);
+list.add(todo3);
+list.add(todo4);
+console.log(list.size());
